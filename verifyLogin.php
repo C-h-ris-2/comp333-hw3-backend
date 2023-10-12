495e4ec37c01131dab27f34e493b6b3e472956b5
cgonzalezt <cristipu123@gmail.com>
2023-10-11 22:15:25 -0400

    Login Page
    Started login page. Verification doesn't fully work :(

---
+++ verifyLogin.php
@@ -0,0 +1,15 @@
+<?php
+include 'connection.php';
+$userid = $_POST['username'];
+$password = $_POST['password'];
+$sql = "SELECT * FROM users WHERE username = '$userid' AND password = '$password'";
+$result = mysqli_query($db, $sql) or die(mysqli_error($db));
+$num = mysqli_fetch_array($result);
+
+if($num > 0) {
+ echo "Login Success";
+}
+else {
+ echo "Wrong User id or password";
+}
+?>
