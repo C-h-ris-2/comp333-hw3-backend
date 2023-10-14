748ab2cc858f767ca654ed3b9ba53f637cf9518d
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-10-14 12:03:03 -0400

    I was able to fix the issue with hashing and verifylogin

--- verifyLogin.php
+++ verifyLogin.php
@@ -4,30 +4,27 @@ if (empty($_POST['userid'])) {
   echo "You need to enter your username<br>";
 }
 else {
-  $userid = mysqli_real_escape_string($db, trim($_POST['userid']));
+  $userid =$_POST['userid'];
 }
 
 if(empty($_POST['password'])) {
   echo "You need to input your password<br>";
 }
 else {
-  $password = mysqli_real_escape_string($db, trim($_POST['password']));
+  $password = trim($_POST['password']);
 }
 // Use placeholders ? for username and password values for the time being.
-$sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+$sql = "SELECT username, password FROM users WHERE username = ?";
 // Construct a prepared statement.
 $stmt = mysqli_prepare($db, $sql);
+
 // Bind the values for username and password that the user entered to the
 // statement AS STRINGS (that is what "ss" means). In other words, the
 // user input is strictly interpreted by the server as data and not as
 // porgram code part of the SQL statement.
-mysqli_stmt_bind_param($stmt, "ss", $userid, $password);
+mysqli_stmt_bind_param($stmt, "s", $userid);
 // Run the prepared statement.
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-$num = mysqli_num_rows($result);
 ?>
-
 <html>
   <head>
     <title>Login Page</title>
@@ -40,11 +37,11 @@ $num = mysqli_num_rows($result);
       <form name="form" action="verifyLogin.php" method="POST">
         <p>
           <label> USER NAME: </label>
-          <input type="text" id="user" name="userid" />
+          <input type="text" id="userid" name="userid" />
         </p>
         <p>
           <label> PASSWORD: </label>
-          <input type="text" id="pass" name="password" />
+          <input type="text" id="password" name="password" />
         </p>
 
         <p>
@@ -54,13 +51,28 @@ $num = mysqli_num_rows($result);
       <a href="register.html">Not Registered? Click here!</a>
     </div>
     <?php
-    if($num > 0) {
-      echo "Login Success";
-     }
-     else {
-      echo "Wrong User id or password";
+      
+      if (mysqli_stmt_execute($stmt) === TRUE) {
+        $result = mysqli_stmt_get_result($stmt);
+        $row = mysqli_fetch_assoc($result);
+        if ($row) {
+          $hashed_password = $row['password'];
+          if (password_verify($password, $hashed_password)) {
+            session_start();
+            session_regenerate_id();
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $userid;
+            echo "Logged in";
+          }
+          else {
+            echo "Invalid Credentials";
+          }
+        }
+      } else {
+        echo "Username not found.";
+      }
+
      
-     }
      ?>
   </body>
 </html>
