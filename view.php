90eafe26e9cd8afb612e3c6d6316a9901ba55469
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-10-16 13:38:46 -0400

    minor bug fixes

--- view.php
+++ view.php
@@ -4,8 +4,6 @@ $id=$_GET['id'];
 $_id = strval($id);
 $sql = "SELECT * FROM ratings WHERE id = $_id";
 $curr_rating = mysqli_query($db, $sql);
-$song=$_POST['username'];
-$rating=$_POST['rating'];
 mysqli_close($db);
 session_start();
 ?>
@@ -19,15 +17,14 @@ session_start();
     <a href="logout.php">Log Out</a>
         <?php
             echo "You are currently logged in as: " . $_SESSION["username"] . ".<br>";
-            if (mysqli_num_rows($result) > 0) {
+            if (mysqli_num_rows($curr_rating) > 0) {
                 // output data of each row
-                while($row = mysqli_fetch_assoc($curr_rat)) {
-                  echo "id: " . $row['id']. " - Artist: " . $row['artist']. " - Song" . $row['song']. " - Rating: " . $row['rating']. "<br>";
+                while($row = mysqli_fetch_assoc($curr_rating)) {
+                  echo "id: " . $row['id']. " - Artist: " . $row['artist']. " - Song: " . $row['song']. " - Rating: " . $row['rating']. "<br>";
                 }
               } else {
                 echo "0 results";
               }
         ?>
-        <p>lol</p>
     </body>
 </html>
