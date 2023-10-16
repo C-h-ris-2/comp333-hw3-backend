d2fd279f838680ed3ae33f9f6029ace74d51c54e
cgonzalezt <cristipu123@gmail.com>
2023-10-16 12:27:29 -0400

    suffering
    The sql fetch does not fully work

--- view.php
+++ view.php
@@ -1,10 +1,13 @@
 <?php
 include 'connection.php';
-$sql = "SELECT * FROM ratings";
-$ratings = mysqli_query($db, "SELECT * FROM ratings");
+$id=$_GET['id'];
+$_id = strval($id);
+$sql = "SELECT * FROM ratings WHERE id = $_id";
+$curr_rating = mysqli_query($db, $sql);
+$song=$_POST['username'];
+$rating=$_POST['rating'];
 mysqli_close($db);
 session_start();
-$id2=$_GET['id'];
 ?>
 
 <!DOCTYPE html>
@@ -13,10 +16,18 @@ $id2=$_GET['id'];
     <title>Music Rating Page</title>
 </head>
     <body>
+    <a href="logout.php">Log Out</a>
         <?php
             echo "You are currently logged in as: " . $_SESSION["username"] . ".<br>";
-            echo "ID: " . $id2. ".<br>";
+            if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                while($row = mysqli_fetch_assoc($curr_rat)) {
+                  echo "id: " . $row['id']. " - Artist: " . $row['artist']. " - Song" . $row['song']. " - Rating: " . $row['rating']. "<br>";
+                }
+              } else {
+                echo "0 results";
+              }
         ?>
-        <a href="logout.php">Log Out</a>
+        <p>lol</p>
     </body>
 </html>
