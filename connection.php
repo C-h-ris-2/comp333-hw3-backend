32e40e497e33146d48bdff5face6390d2a95f57d
cgonzalezt <cristipu123@gmail.com>
2023-10-25 13:39:51 -0400

    First attempt to set up REST API
    Doesn't work. Need to find why!
    
    db name: r_api_revmixer
    
    table
    CREATE TABLE users(
        id int(11) PRIMARY KEY AUTO_INCREMENT,
        username VARCHAR(255),
        password VARCHAR(255)
    );

--- connection.php
+++
@@ -1,9 +0,0 @@
-<!-- Connects database to PHP -->
-<?php
-$db = mysqli_connect("localhost","root","","users_hw2");
-
-if (mysqli_connect_errno()) {
- echo "Failed to connect to MySQL: "
-  . mysqli_connect_error();
-}
-?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<!-- Simple PHP Page to end session (logout) -->
-<?php
-    session_destroy();
-    echo 'Logout successful';
-    header("Location: form.html");   
-?>
