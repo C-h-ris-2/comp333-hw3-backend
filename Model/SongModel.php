463158484812422ae79ae324070bac034bdfa8e0
cdiazherrera <102235823+cdiazherrera@users.noreply.github.com>
2023-11-03 23:51:40 -0400

    Update SongModel.php

--- Model/SongModel.php
+++ Model/SongModel.php
@@ -2,12 +2,13 @@
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
 class SongModel extends Database
 {
+    //grabbing all of the info from ratings
     public function getRatings(){
         return $this->select("SELECT * FROM ratings");
     }
-
+//based on the user input and who is logged in, new rating is input into the table
     public function insertRating($userData){
-
+    
         $sql = "INSERT INTO ratings (username, artist, song, rating) VALUES (?,?,?,?)";
         $stmt = $this->connection->prepare($sql);
         $user = $userData['username'];
@@ -17,7 +18,7 @@ class SongModel extends Database
         $stmt->bind_param('sssi', $user, $artist, $song, $rating);
         $stmt->execute();
     }
-
+//grabs id and user input to make changes to a preexisting entry in the table
     public function updateSong($userData) {
         $sql = 'UPDATE ratings SET artist = ?, song = ?, rating = ? WHERE id = ?';
         $artist = $userData['artist'];
@@ -28,7 +29,7 @@ class SongModel extends Database
         $stmt->bind_param("ssii", $artist, $song, $rating, $id);
         $stmt->execute();
     }
-
+//grabs id to delete rating in the table
     public function deleteSong($userData){
         $id = $userData["id"];
         $sql = "DELETE FROM ratings WHERE id=$id";
