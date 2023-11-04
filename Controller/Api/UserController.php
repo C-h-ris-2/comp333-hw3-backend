03bca0892e1b07b46c09e76765bdd18f5d4a646f
cdiazherrera <102235823+cdiazherrera@users.noreply.github.com>
2023-11-03 23:50:14 -0400

    Update UserController.php

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -6,6 +6,8 @@ class UserController extends BaseController
     /** 
 * "/user/list" Endpoint - Get list of users 
 */
+
+    //function which takes the user input from the frontend and passes to UserModel to verify credentials using SQL statement
     public function loginAction()
     {
         $strErrorDesc = '';
@@ -16,6 +18,7 @@ class UserController extends BaseController
 * Getting model for input
 */
                 $userModel = new UserModel();
+                //grabbing user input
                 $data = json_decode(file_get_contents('php://input'), true);
                 $arrUser = $userModel->getUsers($data);
                     /** 
@@ -46,7 +49,7 @@ class UserController extends BaseController
             );
         }
     }
-
+//register function which inputs new user into the database
     public function createAction() {
         $requestMethod = $_SERVER['REQUEST_METHOD'];
         $strErrorDesc = '';
@@ -77,7 +80,7 @@ class UserController extends BaseController
             );
         }
         }
-
+//displays all of the songs in the ratings table 
     public function songlistAction() {
         $strErrorDesc = '';
         $requestMethod = $_SERVER["REQUEST_METHOD"];
@@ -115,7 +118,7 @@ class UserController extends BaseController
             );
         }
     }
-
+//function to insert a new song based on user input
     public function songinsertAction(){
         $requestMethod = $_SERVER['REQUEST_METHOD'];
         $strErrorDesc = '';
@@ -146,7 +149,7 @@ class UserController extends BaseController
         }
     }
 
-
+//given an id, function is updated to user input
     public function updateSongAction(){
         $requestMethod = $_SERVER['REQUEST_METHOD'];
         $strErrorDesc = '';
@@ -180,7 +183,7 @@ class UserController extends BaseController
             );
         }
     }
-
+//given id, song gets deleted from ratings table
     public function deleteSongAction(){
         $requestMethod = $_SERVER['REQUEST_METHOD'];
         $strErrorDesc = '';
